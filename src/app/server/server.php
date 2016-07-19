6767c3a84825c1201fda574a6585b6d228fb950e
Egor Dyukarev <e.dyukarev@itransition.com>
2016-07-19 19:18:12 +0300

    19-07-2016

---
+++ src/app/server/server.php
@@ -0,0 +1,8 @@
+<?php
+
+/*if(isset($_POST['action']) && !empty($_POST['action'])) {
+    echo json_encode(array("blablabla"=>$variable));
+}*/
+
+echo json_encode(array("blablabla"=>"123"));
+return;
---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,32 @@
+export interface ICustomer {
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    address: string;
+    city: string;
+    state: IState;
+    orderTotal?: number;
+}
+
+export interface IState {
+    abbreviation: string;
+    name: string;
+}
+
+export interface IOrder {
+    customerId: number;
+    orderItems: IOrderItem[];
+}
+
+export interface IOrderItem {
+    id: number;
+    productName: string;
+    itemCost: number;
+}
+
+export interface IPicture {
+    id: number;
+    name: string;
+    title: string;
+}
