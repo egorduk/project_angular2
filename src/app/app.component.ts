dcc5980b4d9aa1cbbd8c74fa5556eb031087c609
Egor Dyukarev <e.dyukarev@itransition.com>
2016-08-04 19:15:32 +0300

    04-08-2016

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { APP_PROVIDERS } from './app.providers';
 
+import {LoggedInRouterOutlet} from './LoggedInOutlet';
+
 @Component({ 
   moduleId: module.id,
   selector: 'app-container',
   template: `<router-outlet></router-outlet>`,
-  directives: [ ROUTER_DIRECTIVES ],
+  directives: [ ROUTER_DIRECTIVES, LoggedInRouterOutlet ],
   providers: [ APP_PROVIDERS ]
 })
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,19 @@ import { CustomerRoutes } from './customer/customer.routes';
 import { LoginComponent } from './auth/login.component';
 //import { HomeComponent } from './home.component';
 
+import {Home} from '../home/home';
+import {Login} from '../login/login';
+import {Signup} from '../signup/signup';
+
 export const App_Routes: RouterConfig = [
 ...CustomersRoutes,
 ...CustomerRoutes,
     { path: 'customers', pathMatch: 'full', redirectTo: '/customers' }, //redirect to home page
     { path: 'login', component: LoginComponent, name: 'Login' },
+    { path: '/', redirectTo: ['/home'] },
+    { path: '/home', component: Home, as: 'Home' },
+    { path: '/login', component: Login, as: 'Login' },
+    { path: '/signup', component: Signup, as: 'Signup' }
    // { path: '/', component: HomeComponent, name: 'Home', useAsDefault: true },
     //{ path: '**', component: PageNotFoundComponent }
 ];
